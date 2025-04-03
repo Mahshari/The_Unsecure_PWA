ff98e739d85c58fec7156bc93c2c23bb732639ec
Mahshari <188180399+Mahshari@users.noreply.github.com>
2025-04-03 19:45:48 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,64 +2,40 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import user_management as dbHandler
+from urllib.parse import urlparse, urljoin
 
 # Code snippet for logging a message
 # app.logger.critical("message")
 
 app = Flask(__name__)
 
+def add_security_headers(response):
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host.url)
+    test_url = urlparse(urljoin(request.host.url, target))
+    return test_url.scheme in ("http", "https") and ref_url.netloc == test_url.netloc
+
 
 @app.route("/success.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 def addFeedback():
-    if request.method == "GET" and request.args.get("url"):
-        url = request.args.get("url", "")
-        return redirect(url, code=302)
-    if request.method == "POST":
-        feedback = request.form["feedback"]
-        dbHandler.insertFeedback(feedback)
-        dbHandler.listFeedback()
-        return render_template("/success.html", state=True, value="Back")
-    else:
-        dbHandler.listFeedback()
-        return render_template("/success.html", state=True, value="Back")
-
-
-@app.route("/signup.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
-def signup():
-    if request.method == "GET" and request.args.get("url"):
-        url = request.args.get("url", "")
-        return redirect(url, code=302)
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        DoB = request.form["dob"]
-        dbHandler.insertUser(username, password, DoB)
-        return render_template("/index.html")
-    else:
-        return render_template("/signup.html")
-
-
-@app.route("/index.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
-@app.route("/", methods=["POST", "GET"])
-def home():
-    if request.method == "GET" and request.args.get("url"):
-        url = request.args.get("url", "")
-        return redirect(url, code=302)
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+@@ -52,12 +59,11 @@ def home():
         isLoggedIn = dbHandler.retrieveUsers(username, password)
         if isLoggedIn:
             dbHandler.listFeedback()
             return render_template("/success.html", value=username, state=isLoggedIn)
+            return render_template("success.html", value=username, state=isLoggedIn)
         else:
             return render_template("/index.html")
+            return render_template("index.html")
     else:
         return render_template("/index.html")
 
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-    app.run(debug=True, host="0.0.0.0", port=5000)
